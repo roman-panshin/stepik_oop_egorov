08344424ac01c1751b0a2dfd4e522afb85765a68
Roman Panshin <panshin13@gmail.com>
2022-08-29 21:08:57 +0300

    step 2.7 Property decorator

--- 2.7.py
+++ 2.7.py
@@ -1,4 +1,6 @@
 # Property decorator
+import hashlib
+import os
 
 
 class BankAccount:
@@ -105,3 +107,79 @@ class Square:
 
 # ---
 
+class User:
+    def __init__(self, name, password):
+        self.name = name
+        self.password = password
+
+    @property
+    def password(self):
+        raise AttributeError('password only change, not show')
+
+    @password.setter
+    def password(self, plaintext):
+        salt = os.urandom(32)
+        self._hashed_password = hashlib.pbkdf2_hmac(
+            "sha256", plaintext.encode("utf-8"), salt, 100_000
+        )
+
+
+#
+# jack = User("Jack", "secret_key")
+# print(jack._hashed_password)
+# jack.password = "new_secret"
+# print(jack._hashed_password)
+
+# ---
+
+class Money:
+    def __init__(self, dollars, cents):
+        self.total_cents = cents + dollars * 100
+
+    @property
+    def dollars(self):
+        return self.total_cents // 100
+
+    @dollars.setter
+    def dollars(self, value):
+        if not isinstance(value, int) or value < 0:
+            print('Error dollars')
+        else:
+            self.total_cents = value * 100 + self.total_cents % 100
+
+    @property
+    def cents(self):
+        return self.total_cents % 100
+
+    @cents.setter
+    def cents(self, value):
+        if not isinstance(value, int) or value < 0 or value >= 100:
+            print('Error cents')
+        else:
+            self.total_cents = value + (self.total_cents // 100) * 100
+
+    def __str__(self):
+        return f"Ваше состояние составляет {self.dollars} долларов {self.cents} центов"
+
+
+# bablo = Money(100, 25)
+#
+# print(bablo.dollars)
+# print(bablo.total_cents)
+#
+# bablo.dollars = 77
+# bablo.cents = 34
+#
+#
+# print(bablo.dollars)
+# print(bablo.total_cents)
+
+
+Bill = Money(101, 99)
+print(Bill)  # Ваше состояние составляет 101 долларов 99 центов
+print(Bill.dollars, Bill.cents)  # 101 99
+print(Bill.total_cents)  # 10199
+Bill.dollars = 123
+print(Bill)  # Ваше состояние составляет 666 долларов 99 центов
+Bill.cents = 100
+print(Bill)  # Ваше состояние составляет 666 долларов 12 центов
