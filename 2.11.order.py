44e0c6ee81e83c87fde50d2384c5cb3f8a4b7400
Roman Panshin <panshin13@gmail.com>
2022-09-09 13:15:04 +0300

    step 2.11 - order done

--- 2.11.order.py
+++ 2.11.order.py
@@ -32,21 +32,23 @@ class User:
 
 
 class Cart:
-    def __int__(self, user):
+    def __init__(self, user):
         self.user = user
         self.goods = {}
         self.__total = 0
 
     def add(self, product, count=1):
-        self.goods[product.name] = count
+        if product not in self.goods:
+            self.goods[product] = 0
+        self.goods[product] += count
         self.__total += product.price * count
 
     def remove(self, product, count=1):
-        if self.goods[product.name] < count:
-            self.__total -= product.price * self.goods[product.name]
-            self.goods[product.name] = 0
+        if self.goods[product] <= count:
+            self.__total -= product.price * self.goods[product]
+            del self.goods[product]
         else:
-            self.goods[product.name] -= count
+            self.goods[product] -= count
             self.__total -= product.price * count
 
     @property
@@ -61,18 +63,56 @@ class Cart:
 
     def print_check(self):
         print('---Your check---')
+        result = []
         for item in self.goods:
-            print(f'{self.}')
-        {Имя
-        товара} {Цена
-        товара} {Количество
-        товара} {Сумма}
-
-        print(f'---Total: {self.total}--- ')
+            result.append(f'{item.name} {item.price} {self.goods[item]} {self.goods[item] * item.price}')
+        print(*sorted(result), sep='\n')
+        print(f'---Total: {self.total}---')
 
 
+billy = User('billy@rambler.ru')
 
+lemon = Product('lemon', 20)
+carrot = Product('carrot', 30)
 
+cart_billy = Cart(billy)
+print(cart_billy.user) # Пользователь billy@rambler.ru, баланс - 0
+cart_billy.add(lemon, 2)
+cart_billy.add(carrot)
+cart_billy.print_check()
+''' Печатает текст ниже
+---Your check---
+carrot 30 1 30
+lemon 20 2 40
+---Total: 70---'''
+cart_billy.add(lemon, 3)
+cart_billy.print_check()
+''' Печатает текст ниже
+---Your check---
+carrot 30 1 30
+lemon 20 5 100
+---Total: 130---'''
+cart_billy.remove(lemon, 6)
+cart_billy.print_check()
+''' Печатает текст ниже
+---Your check---
+carrot 30 1 30
+---Total: 30---'''
+print(cart_billy.total) # 30
+cart_billy.add(lemon, 5)
+cart_billy.print_check()
+''' Печатает текст ниже
+---Your check---
+carrot 30 1 30
+lemon 20 5 100
+---Total: 130---'''
+cart_billy.order()
+''' Печатает текст ниже
+Не хватает средств на балансе. Пополните счет
+Проблема с оплатой'''
+cart_billy.user.deposit(150)
+cart_billy.order() # Заказ оплачен
+print(cart_billy.user.balance) # 20
 
 
 # billy = User('billy@rambler.ru')
