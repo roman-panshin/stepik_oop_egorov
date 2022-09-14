3ac4b5fe5d50972dccf7968307fae27fbad08927
Roman Panshin <panshin13@gmail.com>
2022-09-14 15:54:24 +0300

    3.4 magic methods __eq__, __ne__, __lt__, __le__, __gt__, __ge__

---
+++ 3.4.py
@@ -0,0 +1,78 @@
+# magic methods __eq__, __ne__, __lt__, __le__, __gt__, __ge__
+
+
+class Rectangle:
+
+    def __init__(self, a, b):
+        self.a = a
+        self.b = b
+
+    @property
+    def area(self):
+        return self.a * self.b
+
+    def __eq__(self, other):
+        print('__eq__ call')
+        if isinstance(other, Rectangle):
+            return self.a == other.a and self.b == other.b
+
+    def __lt__(self, other):
+        print('__lt__ call')
+        if isinstance(other, Rectangle):
+            return self.area < other.area
+        elif isinstance(other, (int, float)):
+            return self.area < other
+
+    def __le__(self, other):
+        print('__le__ call')
+        return self == other or self < other
+
+
+# v = Rectangle(1, 2)
+# r = Rectangle(3, 4)
+#
+# print(v == r)
+# print(v != r)
+#
+# print(v <= r)
+#
+# print(v >= r)
+
+class ChessPlayer:
+    def __init__(self, name, surname, rating):
+        self.name = name
+        self.surname = surname
+        self.rating = rating
+
+    def __eq__(self, other):
+        if isinstance(other, ChessPlayer):
+            return self.rating == other.rating
+        elif isinstance(other, (int, float)):
+            return self.rating == other
+        else:
+            return 'Невозможно выполнить сравнение'
+
+    def __gt__(self, other):
+        if isinstance(other, ChessPlayer):
+            return self.rating > other.rating
+        elif isinstance(other, (int, float)):
+            return self.rating > other
+        else:
+            return 'Невозможно выполнить сравнение'
+
+    def __lt__(self, other):
+        if isinstance(other, ChessPlayer):
+            return self.rating < other.rating
+        elif isinstance(other, (int, float)):
+            return self.rating < other
+        else:
+            return 'Невозможно выполнить сравнение'
+
+magnus = ChessPlayer('Carlsen', 'Magnus', 2847)
+ian = ChessPlayer('Ian', 'Nepomniachtchi', 2789)
+print(magnus == 4000) # False
+print(ian == 2789) # True
+print(magnus == ian) # False
+print(magnus > ian) # True
+print(magnus < ian) # False
+print(magnus < [1, 2]) # печатает "Невозможно выполнить сравнениe"
