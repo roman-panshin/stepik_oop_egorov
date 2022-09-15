5693ff332472a36ae4cc314345c0f79dc8c96a2b
Roman Panshin <panshin13@gmail.com>
2022-09-15 15:24:31 +0300

    3.8  polymorphism

---
+++ 3.8.py
@@ -0,0 +1,25 @@
+class UnitedKingdom:
+    @staticmethod
+    def capital():
+        print('London is the capital of Great Britain.')
+
+    @staticmethod
+    def language():
+        print('English is the primary language of Great Britain.')
+
+
+class Spain:
+    @staticmethod
+    def capital():
+        print('Madrid is the capital of Spain.')
+
+    @staticmethod
+    def language():
+        print('Spanish is the primary language of Spain.')
+
+
+obj_uk = UnitedKingdom()
+obj_spa = Spain()
+for country in (obj_spa, obj_uk):
+    country.capital()
+    country.language()
